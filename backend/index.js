0d82750173c4e2cbdc64a0888f00311e168bb1b7
Slobodan Djordjevic <sdjordjevic@razor.rs>
2019-03-17 13:35:10 +0100

    Set production client folder

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,14 @@ const app = require('./config/express');
 // listen to requests
 app.listen(port, () => console.info(`server started on port ${port} (${env})`));
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+    
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    });
+  }
+
 /**
 * Exports express
 * @public
