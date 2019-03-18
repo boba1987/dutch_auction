deae777aed15c65c6de9dbfad49188649cdeb67e
Slobodan Djordjevic <sdjordjevic@razor.rs>
2019-03-18 01:56:26 +0100

    Set error messages

---
+++ client/src/components/AuthForm/index.tsx
@@ -0,0 +1,63 @@
+import React, {useState, useEffect} from 'react';
+import UserForm from '../../components/UserForm';
+import Constants from '../../constants/userConstants';
+import {withRouter, RouteComponentProps} from 'react-router-dom';
+import axios from 'axios';
+
+interface AuthFormProps extends RouteComponentProps {
+    url: string,
+    title: string
+}
+
+export default withRouter((props: AuthFormProps) => {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [error, setError] = useState('');
+  
+  const handleSubmit = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    try {
+      let response = await axios({
+        url: props.url,
+        method: 'POST',
+        data: {
+          userName,
+          password
+        },
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      setLoggedIn(true);
+    } catch (error) {
+      if (error.response.data.errors) {
+        let errors = error.response.data.errors.reduce((accumulator: string, error: {msg: string}) => {
+          return accumulator.concat(error.msg, '. ');
+        }, '');
+
+        return setError(`Error: ${errors}`);
+      }
+
+      setError(`Error: ${error.response.data.message}`);
+    }
+  }
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.name === Constants.password) {
+      setPassword(event.target.value);
+    }
+    if (event.target.name === Constants.userName) {
+      setUserName(event.target.value);
+    }
+  }
+
+  useEffect(() => {
+    if (loggedIn) {
+        props.history.push('/');
+    }
+  }, [loggedIn, error]);
+  
+  return <UserForm title={props.title} onsubmit={handleSubmit} handleInputChange={handleInputChange} error={error}/>;
+});
--- client/src/containers/SignUp/index.tsx
+++ client/src/containers/SignUp/index.tsx
@@ -1,50 +1,6 @@
-import React, {useState, useEffect} from 'react';
-import UserForm from '../../components/UserForm';
-import Constants from '../../constants/userConstants';
-import {withRouter} from 'react-router-dom';
-import axios from 'axios';
+import React from 'react';
+import AuthForm from '../../components/AuthForm';
 
-export default withRouter(({history}) => {
-  const [userName, setUserName] = useState('');
-  const [password, setPassword] = useState('');
-  const [loggedIn, setLoggedIn] = useState(false);
-  const [error, setError] = useState('');
-  
-  const handleSubmit = async (e: React.SyntheticEvent) => {
-    e.preventDefault();
-    try {
-      let response = await axios({
-        url: '/user/register',
-        method: 'POST',
-        data: {
-          userName,
-          password
-        },
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-
-      setLoggedIn(true);
-    } catch (error) {
-      setError(`Error: ${error.response.data.message}`);
-    }
-  }
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.name === Constants.password) {
-      setPassword(event.target.value);
-    }
-    if (event.target.name === Constants.userName) {
-      setUserName(event.target.value);
-    }
-  }
-
-  useEffect(() => {
-    if (loggedIn) {
-      history.push('/');
-    }
-  }, [loggedIn, error]);
-  
-  return <UserForm title="Sign up" onsubmit={handleSubmit} handleInputChange={handleInputChange} error={error}/>;
-});
+export default () => {
+  return <AuthForm title="Sign up" url="/user/register" />
+}
