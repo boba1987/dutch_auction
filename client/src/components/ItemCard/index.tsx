19e71394b1df7128d1b0269a5610e0fe233b2495
Slobodan Djordjevic <sdjordjevic@razor.rs>
2019-03-22 02:04:26 +0100

    Remove countdown timer

--- client/src/components/ItemCard/index.tsx
+++ client/src/components/ItemCard/index.tsx
@@ -5,12 +5,11 @@ import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia, {CardMediaProps} from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { Theme } from '@material-ui/core/styles/createMuiTheme';
 import moment from 'moment';
-const ReactCountdownClock = require('react-countdown-clock');
+import {CountdownTimer} from '../Timer';
 
 const styles = (theme: Theme) => createStyles({
   card: {
@@ -38,24 +37,8 @@ interface ImgMediaCardProps extends WithStyles<typeof styles>{
   lastUpdate: string
 }
 
-interface CardMediaPropsCustom extends CardMediaProps {
-  height: string,
-  alt: string
-}
-
-const CardMediaComponent = (props: CardMediaPropsCustom) => (
-  <CardMedia
-    {...props}
-  />
-);
-
 const ImgMediaCard = (props: ImgMediaCardProps) => {
   const { classes, title, description, status, price, seller, nextUpdate, lastUpdate } = props;
-  const now = moment(moment().format());
-  const next = moment(nextUpdate);
-  console.log(now);
-  console.log(next);
-  console.log(next.diff(now, 'seconds'));
   
   return (
     <Card className={classes.card}>
@@ -86,14 +69,13 @@ const ImgMediaCard = (props: ImgMediaCardProps) => {
               Make a Bid
             </Button>
             </CardActions>
-            Time to price update:
-            <CardActions>
-              <ReactCountdownClock 
-                seconds={next.diff(now, 'seconds')}
-                color="#000"
-                alpha={0.9}
-                size={50} />
-            </CardActions>
+            <Typography component="p">
+              Last price update: 
+              { moment(lastUpdate).format('LLLL') }
+            </Typography>
+            <Typography component="p">
+              Next price update: { moment(nextUpdate).format('LLLL') }
+            </Typography>
           </div>
         ) : ''
       }
