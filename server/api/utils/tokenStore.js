3dd272ab9c935ae724d0ec7b18d9b20c78c878ca
Slobodan Djordjevic <boba_dj1987@hotmail.com>
2019-04-12 20:13:05 +0200

    Update tokenStore.js
    Remove redundant statement

--- server/api/utils/tokenStore.js
+++ server/api/utils/tokenStore.js
@@ -2,14 +2,13 @@ let jwt = require('jsonwebtoken');
 
 exports.checkToken = (req, res, next) => {
   let token = req.headers['x-access-token'] || req.headers['authorization']; // Express headers are auto converted to lowercase
+
   if (token) {
     if (token.startsWith('Bearer ')) {
       // Remove Bearer from string
       token = token.slice(7, token.length);
     }
-  }
-
-  if (token) {
+    
     jwt.verify(token, process.env.SECRET, (err, decoded) => {
       if (err) {
         return res.status(403).json({
